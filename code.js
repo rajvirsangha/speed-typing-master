d3ba45b98bb3eab94bf8cb30028cfdcf066f7d4e
Rajvir Sangha <rajvirsangha@Rajvirs-Mac-mini.local>
2022-01-12 12:45:34 -0800

    initial commit

---
+++ code.js
@@ -0,0 +1,6 @@
+// runs the Speed Typing Master
+var differentWords = Math.round(Math.random() * 19)
+eventsModule.init(60, differentWords);
+
+// console.log appData from the dataModule
+dataModule.returnData();
